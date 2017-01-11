972f351a8681b63a35ada75584df3efdfbebfdaa
Maurice Knoop <mpjwknoop@gmail.com>
2017-01-11 22:42:51 +0100

    Define identifying methods for Event

--- HUEVisualizer/src/nl/mauriceknoop/sanseminar/visualization/Event.java
+++ HUEVisualizer/src/nl/mauriceknoop/sanseminar/visualization/Event.java
@@ -5,4 +5,60 @@ package nl.mauriceknoop.sanseminar.visualization;
  */
 public abstract class Event {
 
+    /**
+     * Indicates whether the {@link #isScripted()} method is overridden by a subclass.
+     */
+    // This is set to true. Within isScripted it is set to false. As long as calls to isScripted arrive and stop at
+    // subclasses this value will remain true. If classes don't override isScripted, the definition of isScripted in
+    // this class is used, which sets it to false. This is not 100% correct seeing that subclasses could call
+    // super.isScripted. It is documented however that one should not, or otherwise accept unexpected results.
+    private boolean isScriptedOverride = true;
+
+    /**
+     * Indicates whether the {@link #isStatic()} method is overridden by a subclass.
+     */
+    // This is set to true. Within isStatic it is set to false. As long as calls to isStatic arrive and stop at
+    // subclasses this value will remain true. If classes don't override isStatic, the definition of isStatic in this
+    // class is used, which sets it to false. This is not 100% correct seeing that subclasses could call super.isStatic.
+    // It is documented however that one should not, or otherwise accept unexpected results.
+    private boolean isStaticOverride = true;
+
+    /**
+     * <p>
+     * Indicates whether firing of the given event should lead to firing of this event. This is to handle some scenarios
+     * in which an event is less strict than the fired event.
+     * </p>
+     * <p>
+     *     For instance, if this event would fire for any job while
+     * the given event is for a specific job. Then if the given event is fired, this event should fire as well. Thus
+     * {@code true} should be returned for this specific example.
+     * </p>
+     * @param event The event that could fire
+     * @return Whether or not the given event would cause this event.
+     */
+    public abstract boolean isFiredBy(Event event);
+
+    /**
+     * Indicates whether this Event occurs within the visualization run. This means that it is executed based on the
+     * state of the visualized script. Do not call this method when overriding it, failure to follow this contract can
+     * lead to unexpected results.
+     * @return the negation of the result of {@link #isStatic} if any subclass overrides that method. Otherwise
+     * {@code false}.
+     */
+    public boolean isScripted() {
+        isScriptedOverride = false;
+        return isStaticOverride && !isStatic();
+    }
+
+    /**
+     * Indicates whether this Event occurs outside the visualization run. This means that it is executed based on the
+     * state of the visualizer, not based on the visualized script. Do not call this method when overriding it, failure
+     * to follow this contract can lead to unexpected results.
+     * @return the negation of the result of {@link #isScripted} if any subclass overrides that method. Otherwise
+     * {@code false}.
+     */
+    public boolean isStatic(){
+        isStaticOverride = false;
+        return isScriptedOverride && !isScripted();
+    }
 }
