914c6eb83101d0a3ed8d4b658fb9802fdb446e46
Maurice Knoop <mpjwknoop@gmail.com>
2017-01-11 21:38:48 +0100

    Define basic settings using command, event, action

---
+++ HUEVisualizer/src/nl/mauriceknoop/sanseminar/visualization/Settings.java
@@ -0,0 +1,15 @@
+package nl.mauriceknoop.sanseminar.visualization;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by Maurice on 11-1-2017.
+ */
+public class Settings {
+
+    private final List<Command> preCommands = new LinkedList<>();
+    private final List<Command> eventCommands = new LinkedList<>();
+    private final List<Command> postCommands = new LinkedList<>();
+
+}
